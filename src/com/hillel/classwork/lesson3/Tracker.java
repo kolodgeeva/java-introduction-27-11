9530f66894a2479dfa0215a2efcf72b6cae83e7b
mkolodzheieva <mariana.kolodzheieva@intellias.com>
2021-12-04 12:27:31 +0200

    Add lesson 3

---
+++ src/com/hillel/classwork/lesson3/Tracker.java
@@ -0,0 +1,17 @@
+package com.hillel.classwork.lesson3;
+
+public class Tracker {
+
+  public static void main(String[] args) {
+
+    int steps = 10;
+    int time = 12;
+
+    float result = (float) steps / time;
+
+    System.out.println(result);
+
+
+  }
+
+}
