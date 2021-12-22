ff6c98ec151c5766b983e1c136f12bee59815200
mariana <kolodgeeva@gmail.com>
2021-12-22 21:26:46 +0200

    add lesson 8

---
+++ src/com/hillel/classwork/lesson8/ArrayReverse.java
@@ -0,0 +1,24 @@
+package com.hillel.classwork.lesson8;
+
+
+import static com.hillel.classwork.lesson8.Array.READER;
+
+import java.io.IOException;
+
+public class ArrayReverse {
+
+  public static void main(String[] args) throws IOException {
+
+    int[] numbers = new int[10];
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = Integer.parseInt(READER.readLine());
+    }
+
+    for (int i = numbers.length - 1; i >= 0; i--) {
+      System.out.println(numbers[i]);
+    }
+
+  }
+
+}
