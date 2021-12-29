4d21f20d2d794d8cd81e4e9ecb482a1c32a9be9a
mariana <kolodgeeva@gmail.com>
2021-12-29 21:21:56 +0200

    add lesson 10

---
+++ src/com/hillel/classwork/lesson10/Drawing.java
@@ -0,0 +1,48 @@
+package com.hillel.classwork.lesson10;
+
+public class Drawing {
+
+  /*
+
+   **********
+   **********
+   **********
+   **********
+   **********
+   **********
+   **********
+   **********
+   **********
+   **********
+
+  #
+  ##
+  ###
+  ####
+  #####
+  ######
+  #######
+  ########
+  #########
+  ##########
+
+   */
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    String str = "*";
+    for (int i = 0; i < 10; i++) {
+      System.out.println(str);
+      str = str + "*";
+    }
+
+  }
+
+}
